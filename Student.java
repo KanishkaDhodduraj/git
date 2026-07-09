56e826fa6b7329d7cf2a80f5b14f0a330c77c6ca
Kanishka Dhodduraj <kanishkadhodduraj2007@gmail.com>
2026-07-09 18:25:03 +0530

    Initial commit

---
+++ Student.java
@@ -0,0 +1,14 @@
+public class Student {
+    String name;
+    int age;
+
+    public Student(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public void display() {
+        System.out.println("Student Name: " + name);
+        System.out.println("Student Age : " + age);
+    }
+}
